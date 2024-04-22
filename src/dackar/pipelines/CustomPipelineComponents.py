883cdc143db9bba29202a39145300d9ad4a8db31
Congjian Wang <congjian.wang@inl.gov>
2024-04-22 10:43:23 -0600

    update

--- src/dackar/pipelines/CustomPipelineComponents.py
+++ src/dackar/pipelines/CustomPipelineComponents.py
@@ -414,6 +414,92 @@ def mergeCCWEntities(doc):
   #   doc = mergeCCWEntities(doc)
   return doc
 
+# Recursive Functions
+# @Language.component("mergeCCWEntities")
+# def mergeCCWEntities(doc):
+#   """
+#     Merge the CCW entities
+
+#     Args:
+#       doc: spacy.tokens.doc.Doc, the processed document using nlp pipelines
+
+#     Returns:
+#       doc: spacy.tokens.doc.Doc, the document after expansion of current entities
+#   """
+#   newEnts = []
+#   isUpdated = False
+#   ents = list(doc.ents)
+#   entID, _ = getEntID()
+#   for i in range(len(ents)-1):
+#     ent1, ent2 = ents[i], ents[i+1]
+#     start = ent1.start
+#     end = ent1.end
+#     label = ent1.label
+#     alias = ent1._.alias
+#     # id = ent1.ent_id
+#     if ent1.ent_id_ == entID and not isUpdated:
+#       if start == 1:
+#         prev = doc[start - 1]
+#         if prev.pos_ in ['NUM']:
+#           start = prev.i
+#       elif start > 1:
+#         prev1, prev2 = doc[start-1], doc[start-2]
+#         if prev1.pos_ in ['NUM']:
+#           start = prev1.i
+#         elif prev1.dep_ in ['punct'] and prev2.pos_ in ['NUM']:
+#           start = prev2.i
+#       if ent2.ent_id_ == entID:
+#         cond = ent1.root.dep_ in ['pobj', 'dobj', 'iobj', 'obj', 'obl', 'oprd'] and ent2.root.dep_ in ['nsubj', 'nsubjpass', 'nsubj:pass']
+#         if not cond:
+#           if end == ent2.start or (end == ent2.start - 1 and doc[end].dep_ in ['punct']):
+#             end = ent2.end
+#             label = ent2.label
+#             if ent2._.alias:
+#               alias = ent2._.alias
+#             # id = ent2.ent_id
+#       if end == ent1.end:
+#         if end < ent1.sent.end - 1:
+#           post1, post2 = doc[end], doc[end+1]
+#           if post1.pos_ in ['NUM']:
+#             end = end + 1
+#           elif post1.dep_ in ['punct'] and post2.pos_ in ['NUM']:
+#             end = end + 2
+#         elif end == ent1.sent.end:
+#           post = doc[end]
+#           if post.pos_ in ['NUM']:
+#             end = end + 1
+
+#       if start != ent1.start or end != ent1.end:
+#         isUpdated = True
+#         newEnt = Span(doc, start, end, label=label)
+#         newEnt._.set('alias', alias)
+#         # print(newEnt, newEnt.ent_id_, newEnt.label_, newEnt._.alias)
+#         newEnts.append(newEnt)
+#         # The following can not resolve span attributes
+
+#         # with doc.retokenize() as retokenizer:
+#         #   attrs = {
+#         #     "tag": newEnt.root.tag,
+#         #     "dep": newEnt.root.dep,
+#         #     "ent_type": label,
+#         #     "ent_id": id,
+#         #     "_": {
+#         #           "alias": alias
+#         #           },
+#         #   }
+#         #   retokenizer.merge(newEnt, attrs=attrs)
+#         # newEnts.append(doc[start:start+1])
+#         # print("======>: ", newEnts[0], newEnts[0]._.alias)
+#     if isUpdated:
+#       break
+
+#   doc.ents = filter_spans(list(doc.ents) +  newEnts)
+#   if isUpdated:
+#     doc = mergeCCWEntities(doc)
+#   return doc
+
+
+
 @Language.component("mergePhrase")
 def mergePhrase(doc):
   """
