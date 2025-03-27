3d605befa4837bc4336f296f8db1493b72948e19
Diego Mandelli <diego.mandelli@inl.gov>
2025-03-27 09:48:02 -0600

    addressed comments

--- examples/demo/data/numeric/signalProfiles.py
+++ examples/demo/data/numeric/signalProfiles.py
@@ -7,6 +7,32 @@ def binaryProfile(freqFact=None,
                   nullLoc=None, nullDur=None,
                   offsetVar=None,
                   impLoc=None,    impVal=None):
+    """
+        This method generates a periodic square wave function with noise included.
+        Orignal parameteres of this function are:
+        * frequency: np.pi * 2
+        * noise: sigma=.03 
+
+        Args:
+
+        freqFact: float, frequency multiplier for an anomaly in the wave frequency
+
+        sigmaAnomaly: float, sigma for an anomaly in the wave noise
+
+        ampAnomaly: float, multiplier for an anomaly in the wave amplitude (prior noise addition)
+
+        nullLoc: integer, location in the [0,500] range for an anomaly where signal is set to 0
+        nullDur: integer, duration value in the [0,500] range for an anomaly where signal is set to 0
+
+        offsetVar: float, vertical offset for an anomaly in the wave signal
+
+        impVal: float, value for a point-value anomaly in the wave noise
+        impLoc: integer, location in the [0,500] range for a point-value anomaly where signal value is set to impVal
+
+        Returns:
+
+        data: np.array, data for a single instance of a periodic square wave function
+    """
 
     t = np.linspace(0, 3, 500, endpoint=False)
     
@@ -36,6 +62,7 @@ def binaryProfile(freqFact=None,
 
     return data  
 
+
 def impulse(fc, bw, sigmaNoise=.05, 
             offset=None,     
             impLoc=None,    impVal=None,
@@ -43,6 +70,36 @@ def impulse(fc, bw, sigmaNoise=.05,
             noiseLoc=None,  noiseDur=None,
             freezeLoc=None, freezeDur=None,
             nullLoc=None,   nullDur=None):
+    """
+        This method generates a Gaussian modulated sinusoid pulse function with noise included.
+        Orignal parameteres of this function are:
+        * frequency: np.pi * 2
+        * noise: sigma=.05
+
+        Args:
+
+        offset: float, vertical offset for an anomaly in the pulse signal
+
+        impVal: float, value for a point-value anomaly in the pulse signal
+        impLoc: integer, location in the [0,500] range for a point-value anomaly where signal value is set to impVal
+
+        offsetVar: float, vertical offset for an anomaly in the pulse  signal
+        offsetLoc: integer, location in the [0,200] range for a vertical offset anomaly
+        offsetDur: integer, duration in the [0,200] range for a vertical offset anomaly
+
+        noiseLoc: integer, location in the [0,200] range for an anomaly where noise is multiplied by 2.0
+        noiseDur: integer, duration in the [0,200] range for a noise anomaly
+
+        freezeLoc: integer, location in the [0,200] range for an anomaly where signal is kept constant to the previous signal value (freeze)
+        freezeDur: integer, duration in the [0,200] range for a freeze anomaly
+
+        nullLoc: integer, location in the [0,200] range for an anomaly where signal is set to 0
+        nullDur: integer, duration value in the [0,200] range for an anomaly where signal is set to 0
+
+        Returns:
+
+        data: np.array, data for a single instance of a Gaussian modulated sinusoid pulse function
+    """
 
     t = np.linspace(1.5, -1.5, 200, endpoint=False)
 
@@ -72,15 +129,38 @@ def impulse(fc, bw, sigmaNoise=.05,
 
     return signal
 
-def HS(x):
-    return np.heaviside(x, 0)
 
 def yearProfile(impLoc=None,    impFac=None, 
                 freezeLoc=None, freezeDur=None, 
                 noiseLoc=None,  noiseDur=None,
                 offsetLoc=None, offsetDur=None,
                 driftLoc=None,  driftDur= None, driftFact=None):
+    """
+        This method generates a sinusoidal function with noise included that emulates yearly seasonality.
+        Orignal parameteres of this function are:
+        * frequency: np.pi * 2
+        * noise: sigma = .05 for the first half of the signal
+                 sigma = .25 for the second half of the signal
 
+        Args:
+
+        freezeLoc: integer, location in the [0,365] range for an anomaly where signal is kept constant to the previous signal value (freeze)
+        freezeDur: integer, duration in the [0,365] range for a freeze anomaly
+
+        noiseLoc: integer, location in the [0,365] range for an anomaly where noise is multiplied by 2.4
+        noiseDur: integer, duration in the [0,365] range for a noise anomaly
+
+        offsetLoc: integer, location in the [0,365] range for a vertical offset anomaly of .2
+        offsetDur: integer, duration in the [0,365] range for a vertical offset anomaly
+
+        driftLoc: integer, location in the [0,365] range for a drift anomaly 
+        driftDur: integer, duration in the [0,365] range for a drift anomaly 
+        driftFact: float, coefficient for the drift anomaly 
+
+        Returns:
+
+        data: np.array, data for a single instance of a periodic square wave function
+    """
     length = 365
     t = np.arange(length)
     signal = -np.sin(2 * np.pi * t / length)
@@ -91,7 +171,7 @@ def yearProfile(impLoc=None,    impFac=None,
     if offsetLoc is not None:
         signal[offsetLoc:(offsetLoc+offsetDur)] = signal[offsetLoc:(offsetLoc+offsetDur)] + .2
 
-    noise = np.random.normal(0,.05,length) + signal*HS(signal)*np.random.normal(0,.2,length)
+    noise = np.random.normal(0,.05,length) + signal * np.heaviside(signal,0) * np.random.normal(0,.2,length)
 
     if noiseLoc is not None:
         noise[noiseLoc:(noiseLoc+noiseDur)] = noise[noiseLoc:(noiseLoc+noiseDur)]*2.4
--- src/dackar/utils/mbse/customMBSEparser.py
+++ src/dackar/utils/mbse/customMBSEparser.py
@@ -8,6 +8,9 @@ Created on February, 2024
 
 # External Imports
 import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
 
 class customMBSEobject(object):
     """
@@ -29,7 +32,7 @@ class customMBSEobject(object):
         self.nodes_filename = nodes_filename
         self.edges_filename = edges_filename
         self.entities = []
-        self.listIDs = []
+        self.list_IDs = []
 
         self.allowed_node_types = ['entity']
         self.allowed_edge_types = ['link','composition','support'] # to be developed: 'opm_instance'
@@ -74,32 +77,32 @@ class customMBSEobject(object):
 
             None
         """
-        print('- Check node file -')
+        logger.info('- Check node file -')
         # Check all columns are present
         cols = self.nodes_df.columns.tolist()
         if set(cols)!=set(self.allowed_node_cols):
             raise IOError('Node file structure check - Error: wrong set of provided columns (allowed: nodeID, label, ID, type)')
         else:
-            print('Node file structure check - Pass')
+             logger.info('Node file structure check - Pass')
 
         # Check for duplicate IDs
         duplicateIDs = self.nodes_df.duplicated()
 
         if self.nodes_df[duplicateIDs].empty:
-            print("List of node IDs check - Pass")
+             logger.info("List of node IDs check - Pass")
         else:
-            print("List of node IDs check - Error: duplicate IDs were found:")
-            print(self.nodes_df[duplicateIDs])
+             logger.info("List of node IDs check - Error: duplicate IDs were found:")
+             logger.info(self.nodes_df[duplicateIDs])
         
         #check for structure of each row
-        print("Entity check...")
+        logger.info("Entity check...")
         for index, row in self.nodes_df.iterrows():
             if row['type'] not in set(self.allowed_node_types):
                 raise IOError('Type of row ' + str(index) + ' in node file is not allowed. Allowed types: ' +str(self.allowed_node_types))
             
             if pd.isnull(row['type']) and pd.isnull(row['ID']):
                 raise IOError('Entity of row ' + str(index) + ' in node file: Error - neither type nor ID have been specified')
-        print("Entities check: Pass")
+        logger.info("Entities check: Pass")
 
     def checkEdgesFile(self):
         """
@@ -113,22 +116,22 @@ class customMBSEobject(object):
 
             None
         """
-        print('- Check edge file -')
+        logger.info('- Check edge file -')
         # Check all columns are present
         cols = self.edges_df.columns.tolist()
         if set(cols)!=set(self.allowed_edge_cols):
             raise IOError('Edge file structure check - Error: wrong set of provided columns (allowed: sourceNodeId,targetNodeId,type,medium)')
         else:
-            print('Edge file structure check - Pass')
+             logger.info('Edge file structure check - Pass')
 
         # Check for duplicate edges
         duplicateEdges = self.edges_df[['sourceNodeId','targetNodeId']].duplicated()
 
         if self.edges_df[duplicateEdges].empty:
-            print("List of edges check - Pass")
+             logger.info("List of edges check - Pass")
         else:
-            print("List of edges check - Error: duplicate edges were found:")
-            print(self.edges_df[duplicateEdges])
+            logger.info("List of edges check - Error: duplicate edges were found:")
+            logger.info(self.edges_df[duplicateEdges])
 
         # Check IDs in edge file are defined in node file
         sourceNodeId_list = self.edges_df['sourceNodeId'].to_list()
@@ -142,22 +145,22 @@ class customMBSEobject(object):
             raise IOError('Error - Edge file: not recognized entities: ' + str(diff2))
 
         # Check for structure of each row
-        print("Edges check...")
+        logger.info("Edges check...")
         for index, row in self.edges_df.iterrows():
             if pd.isnull(row['sourceNodeId']) or pd.isnull(row['targetNodeId']):
-                print(row)
+                logger.info(row)
                 raise IOError('Edge ' + str(index) + ' in edge file: Error - both sourceNodeId and targetNodeId need to be specified')
              
             if row['type'] not in set(self.allowed_edge_types):
-                print(row)
+                logger.info(row)
                 raise IOError('Type of row ' + str(index) + ' in edge file is not allowed. Allowed types: ' +str(self.allowed_edge_types))
             
             if row['type']=='link' and pd.isnull(row['medium']):
-                print(row)
+                logger.info(row)
                 raise IOError('Edge ' + str(index) + ' in edge file: Error - link does not have a medium specified')
 
             if row['type']=='support' and not pd.isnull(row['medium']):
-                print(row)
+                logger.info(row)
                 raise IOError('Edge ' + str(index) + ' in edge file: Error - support does not support medium keyword; specified:' +str(row['medium']))
 
         # check that entities in the node file have been mentioned in edge file
@@ -165,13 +168,13 @@ class customMBSEobject(object):
         diff3 = set(self.entities) - set(entities_edge_list)
         if diff3:
             raise IOError('Error - Node file: these entities in the node file were not mentioned in the edge file: ' + str(diff3))        
-        print("Edges check: Pass")
+        logger.info("Edges check: Pass")
         
         # Provide info of outgoing only nodes
         outgoingSet = set(sourceNodeId_list) - set(targetNodeId_list)
-        print('List of outgoing only nodes:' + str(outgoingSet))
+        logger.info('List of outgoing only nodes:' + str(outgoingSet))
         # Provide info of ingoing only nodes
-        outgoingSet = set(targetNodeId_list) - set(sourceNodeId_list)
-        print('List of ingoing only nodes:' + str(outgoingSet))
+        ingoingSet = set(targetNodeId_list) - set(sourceNodeId_list)
+        logger.info('List of ingoing only nodes:' + str(ingoingSet))
 
         
