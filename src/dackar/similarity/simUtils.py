fix word order similarity calculation

src/dackar/similarity/simUtils.py
@@ -22,7 +22,7 @@ from .synsetUtils import semanticSimilarityUsingDisambiguatedSynsets
   Codes are modified from https://github.com/anishvarsha/Sentence-Similaritity-using-corpus-statistics
 """
 
-def sentenceSimilarity(sentenceA, sentenceB, infoContentNorm=False, delta=0.85):
+def sentenceSimilarity(sentenceA, sentenceB, infoContentNorm=True, delta=0.85):
   """
     Compute sentence similarity using both semantic and word order similarity
     The semantic similarity is based on maximum word similarity between one word and another sentence
@@ -46,6 +46,8 @@ def sentenceSimilarity(sentenceA, sentenceB, infoContentNorm=False, delta=0.85):
 def wordOrderSimilaritySentences(sentenceA, sentenceB):
   """
     Compute sentence similarity using word order similarity
+    Ref: Li, Yuhua, et al. "Sentence similarity based on semantic nets and corpus statistics."
+    IEEE transactions on knowledge and data engineering 18.8 (2006): 1138-1150.
 
     Args:
 
@@ -59,13 +61,12 @@ def wordOrderSimilaritySentences(sentenceA, sentenceB):
   wordsA = tokenizer(sentenceA.lower())
   wordsB = tokenizer(sentenceB.lower())
   wordSet = combineListsRemoveDuplicates(wordsA, wordsB)
-  index = {word[1]: word[0] for word in enumerate(wordSet)}
-  r1 = constructWordOrderVector(wordsA, wordSet, index)
-  r2 = constructWordOrderVector(wordsB, wordSet, index)
+  r1 = constructWordOrderVector(wordsA, wordSet)
+  r2 = constructWordOrderVector(wordsB, wordSet)
   srTemp = np.linalg.norm(r1-r2)/np.linalg.norm(r1+r2)
   return 1-srTemp
 
-def constructWordOrderVector(words, jointWords, index):
+def constructWordOrderVector(words, jointWords):
   """
     Construct word order vector
 
@@ -73,31 +74,32 @@ def constructWordOrderVector(words, jointWords, index):
 
       words: set of words, a set of words for one sentence
       jointWords: set of joint words, a set of joint words for both sentences
-      index: dict, word index in the joint set of words
 
     Returns:
 
       vector: numpy.array, the word order vector
   """
   vector = np.zeros(len(jointWords))
-  i = 0
-  wordSet = set(words)
-  for jointWord in jointWords:
-    if jointWord in wordSet:
-      vector[i] = index[jointWord]
-    else:
-      wordSimilar, similarity = identifyBestSimilarWordFromWordSet(jointWord, wordSet)
-      if similarity > 0.4:
-        vector[i] = index[wordSimilar]
+  for i, jointWord in enumerate(jointWords):
+    try:
+      index = words.index(jointWord)
+    except ValueError:
+      # Additional enhancement
+      # If the words similarity score is very high, the words will be treated the same.
+      wordSimilar, similarity = identifyBestSimilarWordFromWordSet(jointWord, set(words))
+      if similarity > 0.9:
+        index = words.index(wordSimilar)
       else:
-        vector[i] = 0
-    i +=1
+        index = 0
+    vector[i] = index
   return vector
 
 def semanticSimilaritySentences(sentenceA, sentenceB, infoContentNorm):
   """
     Compute sentence similarity using semantic similarity
     The semantic similarity is based on maximum word similarity between one word and another sentence
+    Ref: Li, Yuhua, et al. "Sentence similarity based on semantic nets and corpus statistics."
+    IEEE transactions on knowledge and data engineering 18.8 (2006): 1138-1150.
 
     Args:
 

src/dackar/similarity/synsetUtils.py
@@ -39,14 +39,12 @@ def wordOrderSimilaritySynsetList(synsetList1, synsetList2):
   """
   # keep the order (works for python3.7+)
   synSet = list(dict.fromkeys(synsetList1+synsetList2))
-  # synSet = list(set(synsetList1).union(set(synsetList2)))
-  index = {syn[1]: syn[0] for syn in enumerate(synSet)}
-  r1 = constructSynsetOrderVector(synsetList1, synSet, index)
-  r2 = constructSynsetOrderVector(synsetList2, synSet, index)
+  r1 = constructSynsetOrderVector(synsetList1, synSet)
+  r2 = constructSynsetOrderVector(synsetList2, synSet)
   srTemp = np.linalg.norm(r1-r2)/np.linalg.norm(r1+r2)
   return 1-srTemp
 
-def constructSynsetOrderVector(synsets, jointSynsets, index):
+def constructSynsetOrderVector(synsets, jointSynsets):
   """
     Construct synset order vector for word order similarity calculation
 
@@ -54,25 +52,22 @@ def constructSynsetOrderVector(synsets, jointSynsets, index):
 
       synsets: list of synsets
       jointSynsets: list of joint synsets
-      index: int, index for synsets
 
     Returns:
 
       vector: np.array, synset order vector
   """
   vector = np.zeros(len(jointSynsets))
-  i = 0
-  synsets = set(synsets)
-  for syn in jointSynsets:
-    if syn in synsets:
-      vector[i] = index[syn]
-    else:
-      synSimilar, similarity = identifyBestSimilarSynsetFromSynsets(syn, synsets)
-      if similarity > 0.4:
-        vector[i] = index[synSimilar]
+  for i, syn in enumerate(jointSynsets):
+    try:
+      index = synsets.index(syn)
+    except ValueError:
+      synSimilar, similarity = identifyBestSimilarSynsetFromSynsets(syn, set(synsets))
+      if similarity > 0.9:
+        index = synsets.index(synSimilar)
       else:
-        vector[i] = 0
-    i +=1
+        index = 0
+    vector[i] = index
   return vector
 
 def identifyBestSimilarSynsetFromSynsets(syn, synsets):