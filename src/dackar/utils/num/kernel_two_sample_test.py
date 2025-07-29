49cb8fea6263ed7f696c96ac6399713613b6729b
Diego Mandelli <diego.mandelli@inl.gov>
2025-07-29 12:35:28 -0600

    addedc

---
+++ src/dackar/utils/num/kernel_two_sample_test.py
@@ -0,0 +1,224 @@
+# Copyright 2024, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
+"""
+Created on July, 2025
+
+@author: wangc, mandd
+"""
+
+# Code modified from https://github.com/emanuele/kernel_two_sample_test
+import numpy as np
+from sys import stdout
+from sklearn.metrics import pairwise_kernels
+
+
+def MMD2u(K, m, n):
+    """
+        The MMD^2_u unbiased statistic using U-statistics.
+    """
+    Kx = K[:m, :m]
+    Ky = K[m:, m:]
+    Kxy = K[:m, m:]
+    return 1.0 / (m * (m - 1.0)) * (Kx.sum() - Kx.diagonal().sum()) + \
+        1.0 / (n * (n - 1.0)) * (Ky.sum() - Ky.diagonal().sum()) - \
+        2.0 / (m * n) * Kxy.sum()
+
+def MMD2b(K, m, n):
+    """
+        The MMD^2 biased statistics using V-statistics
+    """
+    Kx = K[:m, :m]
+    Ky = K[m:, m:]
+    Kxy = K[:m, m:]
+    out = 1.0/(m**2) * Kx.sum() + 1.0/(n**2)*Ky.sum() - 2.0/(m*n)*Kxy.sum()
+    return out
+
+def MMD2u_UCB(K, m, alpha=0.05):
+    """
+        Uniform convergence bound for MMD2u
+    """
+    assert alpha > 0 and alpha < 1, f'alpha should be within [0,1], but got {alpha}'
+    Kxy = K[:m, m:]
+    maxKxy = np.max((0,np.max(Kxy)))
+    ucb = 4.0*maxKxy/np.sqrt(m) * np.sqrt(np.log(1.0/alpha))
+    return ucb
+
+
+def MMD2b_UCB(K, m, alpha=0.05):
+    """
+        Uniform convergence bound for MMD2b
+    """
+    assert alpha > 0 and alpha < 1, f'alpha should be within [0,1], but got {alpha}'
+    Kxy = K[:m, m:]
+    maxKxy = np.max((0,np.max(Kxy)))
+    ucb = np.sqrt(2.0*maxKxy/m) * (1.0+np.sqrt(2.0*np.log(1.0/alpha)))
+    return ucb
+
+
+def compute_null_distribution(K, m, n, iterations=1000, verbose=False,
+                              random_state=None, marker_interval=500):
+    """Compute the bootstrap null-distribution of MMD2u.
+    """
+    if type(random_state) == type(np.random.RandomState()):
+        rng = random_state
+    else:
+        rng = np.random.RandomState(random_state)
+
+    mmd2u_null = np.zeros(iterations)
+    for i in range(iterations):
+        if verbose and (i % marker_interval) == 0:
+            print(i),
+            stdout.flush()
+        idx = rng.permutation(m+n)
+        K_i = K[idx, idx[:, None]]
+        mmd2u_null[i] = MMD2u(K_i, m, n)
+    return mmd2u_null
+
+
+def compute_null_distribution_given_permutations(K, m, n, permutation,
+                                                 iterations=None):
+    """Compute the bootstrap null-distribution of MMD2u given
+    predefined permutations.
+
+    Note:: verbosity is removed to improve speed.
+    """
+    if iterations is None:
+        iterations = len(permutation)
+
+    mmd2u_null = np.zeros(iterations)
+    for i in range(iterations):
+        idx = permutation[i]
+        K_i = K[idx, idx[:, None]]
+        mmd2u_null[i] = MMD2u(K_i, m, n)
+
+    return mmd2u_null
+
+
+def kernel_two_sample_test(X, Y, kernel_function='rbf', iterations=2000,
+                           verbose=False, random_state=None, alpha=0.05, thin=None, **kwargs):
+    """Compute MMD^2_u, its null distribution and the p-value of the
+    kernel two-sample test.
+
+    Note that extra parameters captured by **kwargs will be passed to
+    pairwise_kernels() as kernel parameters. E.g. if
+    kernel_two_sample_test(..., kernel_function='rbf', gamma=0.1),
+    then this will result in getting the kernel through
+    kernel_function(metric='rbf', gamma=0.1).
+
+    valid values for kernel_function are:
+    [additive_chi2, chi2, linear, poly, polynomial, rbf, laplacian, sigmoid, cosine]
+    """
+
+    if len(X.shape) == 1:
+        X = X.reshape((-1,1))
+    if len(Y.shape) == 1:
+        Y = Y.reshape((-1,1))
+    if thin is not None:
+        try:
+            thin = int(thin)
+        except ValueError:
+            print(f'Try to perform thin calculation, but get non-integer value for "thin: {thin}"')
+            thin = None
+    if thin is not None:
+        X = X[0::thin, :]
+        Y = Y[0::thin, :]
+    m = len(X)
+    n = len(Y)
+    XY = np.vstack([X, Y])
+    K = pairwise_kernels(XY, metric=kernel_function, **kwargs)
+    mmd2u = MMD2u(K, m, n)
+
+    if verbose:
+        print("MMD^2_u = %s" % mmd2u)
+        print("Computing the null distribution.")
+    ucb = MMD2u_UCB(K, m, alpha=0.05)
+    if verbose:
+        print(f"UCB bound for MMD^2_u is {ucb}")
+    mmd2u_null = compute_null_distribution(K, m, n, iterations,
+                                           verbose=verbose,
+                                           random_state=random_state)
+    p_value = max(1.0/iterations, (mmd2u_null > mmd2u).sum() /
+                  float(iterations))
+    if verbose:
+        print("p-value ~= %s \t (resolution : %s)" % (p_value, 1.0/iterations))
+        if p_value < alpha:
+            print(f"Deviaition from null hyothesis is statistically significant at test level {alpha}, reject null hyothesis, \n \
+                  i.e., the two samples are from different distributions!")
+
+    return mmd2u, mmd2u_null, p_value
+
+def chebyshevTesting(X, Y, kernel_function='rbf', iterations=2000,
+                           verbose=False, random_state=None, alpha=0.01, **kwargs):
+    """
+        Chebyshev Testing using Chebyshev's Inequality
+    """
+    accept = False
+    mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y, kernel_function=kernel_function, iterations=iterations,
+                           verbose=verbose, random_state=random_state, alpha=alpha, **kwargs)
+    accept = chebyshevTesting_precomputed_mmd(mmd2u, mmd2u_null, alpha)
+    return accept
+
+def chebyshevTesting_precomputed_mmd(mmd2u, mmd2u_null, alpha=0.01):
+    """
+        Chebyshev Testing using Chebyshev's Inequality
+    """
+    accept = False
+    mu = mmd2u_null.mean()
+    sigma = np.std(mmd2u_null)
+    nu = 1.0/(np.sqrt(alpha))
+    diff = abs(mmd2u-mu)
+    if diff < nu * sigma:
+        accept = True
+        print(f"Chebyshev Inequality holds: {diff} < {nu*sigma}")
+    else:
+        accept = False
+        print(f"Chebyshev Inequality does not hold: {diff} > {nu*sigma}")
+    return accept
+
+if __name__ == '__main__':
+
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import pairwise_distances
+
+    np.random.seed(0)
+
+    m = 200
+    n = 200
+    d = 2
+
+    sigma2X = np.eye(d)
+    muX = np.zeros(d)
+
+    sigma2Y = np.eye(d)
+    # muY = np.ones(d)
+    muY = np.zeros(d)
+
+    iterations = 2000
+
+    X = np.random.multivariate_normal(mean=muX, cov=sigma2X, size=m)
+    Y = np.random.multivariate_normal(mean=muY, cov=sigma2Y, size=n)
+
+    if d == 2:
+        plt.figure()
+        plt.plot(X[:, 0], X[:, 1], 'bo')
+        plt.plot(Y[:, 0], Y[:, 1], 'rx')
+
+    sigma2 = np.median(pairwise_distances(X, Y, metric='euclidean'))**2
+    mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y,
+                                                        kernel_function='rbf',
+                                                        iterations=iterations,
+                                                        gamma=1.0/sigma2,
+                                                        verbose=True)
+    # mmd2u, mmd2u_null, p_value = kernel_two_sample_test(X, Y,
+    #                                                     kernel_function='linear',
+    #                                                     verbose=True)
+
+    plt.figure()
+    prob, bins, patches = plt.hist(mmd2u_null, bins=50) #, normed=True
+    plt.plot(mmd2u, prob.max()/30, 'w*', markersize=24, markeredgecolor='k',
+             markeredgewidth=2, label="$MMD^2_u = %s$" % mmd2u)
+    plt.xlabel('$MMD^2_u$')
+    plt.ylabel('$p(MMD^2_u)$')
+    plt.legend(numpoints=1)
+    plt.title('$MMD^2_u$: null-distribution and observed value. $p$-value=%s'
+              % p_value)
+    plt.show()
---
+++ src/dackar/utils/num/t_score.py
@@ -0,0 +1,345 @@
+# Copyright 2024, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
+
+"""
+Created on July, 2025
+
+@author: wangc, mandd
+"""
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.metrics import pairwise_distances
+
+import logging
+logger = logging.getLogger(__name__)
+
+from .kernel_two_sample_test import kernel_two_sample_test
+
+
+def getL(loc,window,array,type):
+    """
+      Method designed to extract a portion of a time series
+
+      Args:
+        loc: int, reference location in the time series for the selected portion
+        window: np array, size of the selected portion of the time series
+        array: np array, original time series
+        type: string, type of the selected portion of the time series (rear or front)
+      Return:
+        timeseriesPortion: np array, size of the selected portion of the time series
+    """
+    if type not in ['rear','front']:
+        logger.error('getL method: Specificed type to select portion of time series (' + str (type) + ') is not allowed. Type can be either rear or front.')
+    
+    if isinstance(array, np.ndarray) or isinstance(array, pd.Series):
+        if type=='rear':
+            l = array[loc:loc+window]
+        elif type=='front':
+            l = array[loc-window:loc]
+        else:
+            print('error')
+    else:
+        logger.error('getL method: Provided time series has incorrect data type.')
+    
+    timeseriesPortion = np.asarray(l)
+    return timeseriesPortion
+
+
+def omega(window,array,Nsamples):
+    """
+      Method designed to extract set of portions of a time series
+
+      Args:
+        window: np array, size of the selected portion of the time series
+        array: np array, original time series
+        Nsamples: int, number of portions to be selected
+      Return:
+        omegaSet: np.array, set of Nsamples portions of a time series
+    """
+    size = len(array)
+    if isinstance(array, np.ndarray) or isinstance(array, list):
+        omegaSet=np.zeros([Nsamples,window])
+        start = np.random.randint(low=0, high=(size-window), size=Nsamples)
+        for i in range(Nsamples):
+            omegaSet[i,:] = array[start[i]:start[i]+window]
+    elif isinstance(array, pd.Series):
+        omegaSet = []
+        # pd.Series with Date as index, the length with window is not consistent
+        # omegaSet=np.zeros([Nsamples,len(array[:array.index[0]+window])])
+        high = len(array[:array.index[-1]-window])
+        start = np.random.randint(low=0, high=high, size=Nsamples)
+        for i in range(Nsamples):
+            loc = array.index[start[i]]
+            sample = array[loc:loc+window]
+            omegaSet.append(np.asarray(sample))
+    return omegaSet
+
+def choice(array,Nsamples):
+    """
+      Method designed to randomly choose Nsamples out of array (replace is set to True)
+
+      Args:
+        array: np.array, array of values to be sampled
+        Nsamples: int, number of elements of array to be selected
+      Return:
+        sampled: np.array, set of Nsamples elements randomly chosen from array
+    """
+    sampled = np.random.choice(array, size=Nsamples, replace=True)
+    return sampled
+
+def t_score(array_front, array_rear):
+    """
+      Method designed to calculate the statistical difference between two arrays using t-test
+
+      Args:
+        array_front: np.array, first array
+        array_rear: np.array, second array
+      Return:
+        tscore: float, outcome of t-test
+    """
+
+    if array_front.size == array_rear.size:
+        n = float(array_front.size)
+
+        mean_front = np.mean(array_front)
+        var_front  = np.var(array_front)
+
+        mean_rear = np.mean(array_rear)
+        var_rear  = np.var(array_rear)
+
+        tscore = (mean_front-mean_rear)/math.sqrt((var_front+var_rear)/n)
+    else:
+        print("t_score error: the two provided array have different sizes.")
+        tscore = None
+    return tscore
+
+def MMD_test(test_array, omegaSet, iterations, alphaTest, alphaOmegaset, printFlag=True):
+    """
+      Method designed to calculate the statistical difference between an array and a set of arrays 
+      using the Maximum Mean Discrepancy testing method
+
+      Args:
+        omegaSet: np.ndarray or list, population of arrays
+        test_array: np.array, array of values to be tested against omegaSet
+        iterations: int, number of iterartions required to compute MMD^2_u
+        alphaTest: float, acceptance value for hypothesis testing (single array testing)
+        alphaOmegaset: float, acceptance value for hypothesis testing (omegaSet testing)
+        printFlag: bool, flag to plot MMD distribution
+      Return:
+        p_value: float, p-value obtained from MMD testing
+        testOutput: bool, logical outcome of MMD testing
+    """
+
+    if isinstance(omegaSet, np.ndarray):
+        omegaSetDim  = omegaSet.ndim
+    elif isinstance(omegaSet, list):
+        if isinstance(omegaSet[0], np.ndarray):
+            omegaSetDim = len(omegaSet)
+        else:
+            omegaSetDim = 1
+
+    if omegaSetDim==1:
+        sigma2 = np.median(pairwise_distances(test_array.reshape(-1, 1), omegaSet.reshape(-1, 1), metric='euclidean'))**2
+        mmd2u, mmd2u_null, p_value = kernel_two_sample_test(test_array, omegaSet,
+                                                            kernel_function='rbf',
+                                                            iterations=iterations,
+                                                            gamma=1.0/sigma2,
+                                                            verbose=False)
+        # print("Null hypothesis: the two sub-series are generated from the same distribution")
+        if p_value < alphaTest:
+            nullHypothesis = False
+            testOutput = True
+            print(f'The p-value is: {round(p_value, 5)}, which is less than the significant level: {alphaTest}')
+            # print("The null hypothesis got rejected")
+            # print('The two sub-series are generated from two different distributions')
+        else:
+            nullHypothesis = True
+            testOutput = False
+            print(f'The p-value is: {round(p_value, 5)}, which is larger than the significant level: {alphaTest}')
+            # print("The null hypothesis got accepted")
+            # print('The two sub-series are generated from the same distributions')
+        return p_value, testOutput
+    else:
+        omegaSetSize = len(omegaSet)
+        mmd2u_arr    = np.zeros(omegaSetSize)
+        mmd2u_null_agg = []
+        p_value_arr  = np.zeros(omegaSetSize)
+        if isinstance(omegaSet, np.ndarray):
+            sigma2 = np.std(omegaSet.ravel())**2
+        elif isinstance(omegaSet, list):
+            ravelData = []
+            for data in omegaSet:
+                ravelData.extend(data)
+            sigma2 = np.std(ravelData)**2
+        for i in range(omegaSetSize):
+            # sigma2 = np.median(pairwise_distances(test_array.reshape(-1, 1), omegaSet[i,:].reshape(-1, 1), metric='euclidean'))**2
+            if isinstance(omegaSet, np.ndarray):
+                data = omegaSet[i, :]
+            else:
+                data = omegaSet[i]
+            mmd2u_arr[i], mmd2u_null, p_value_arr[i] = kernel_two_sample_test(test_array, data,
+                                                                              kernel_function='rbf',
+                                                                              iterations=iterations,
+                                                                              gamma=1.0/sigma2,
+                                                                              verbose=False)
+            mmd2u_null_agg.extend(list(mmd2u_null))
+        # if printFlag:
+        #     fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(20, 6))
+        #     ax1.hist(mmd2u_arr,   bins=30, alpha=0.35, density=True, color='green', label='mmd2u')
+        #     ax1.legend()
+
+        #     ax2.hist(p_value_arr, bins=30, alpha=0.35, density=True, color='red', label='p_value')
+        #     ax2.legend()
+
+        #     ax3.scatter(mmd2u_arr, p_value_arr, marker='.')
+        #     ax3.set_xlabel('mmd2u')
+        #     ax3.set_ylabel('p_value')
+
+        #     plt.show()
+
+
+        # the number of rejections for the null hypothesis in two sample testing
+        count = (p_value_arr<alphaTest).sum()
+        # print('Number of rejection ratio for the null hypothesis in two sample testing for two given sub-series is:', count/omegaSetSize)
+        # probability two signals are from the same distribution
+        p_value = 1. - count/omegaSetSize
+        # Null hypothesis: the sub-series are generated from the normal conditions of full time-series
+        # if the probability lower than alphaOmegaset, reject the null hypothesis
+        # print("Null hypothesis: the sub-series are generated from the normal conditions of full time-series")
+
+        if p_value < alphaOmegaset:
+            nullHypothesis = False
+            testOutput = True
+            print(f"The probability of the sub-series are generated from the normal conditions of full time-series is {p_value}, which is smaller than given test value {alphaOmegaset}")
+            # print("The null hypothesis got rejected")
+            # print('The sub-series is correlated to the anomaly events')
+        else:
+            nullHypothesis = True
+            testOutput = False
+            print(f"The probability of the sub-series are generated from the normal conditions of full time-series is {p_value}, which is greater than given test value {alphaOmegaset}")
+            # print("The null hypothesis got accepted")
+            # print('The sub-series is not correlated to the anomaly events')
+
+        if printFlag:
+            fig, ax = plt.subplots()
+            bin1 =30
+            ax.hist(mmd2u_null_agg, bins=bin1, alpha=1.0, density=True, color='red', label='$MMD^2_u$ Null Distribution', stacked=True)
+            bin2 = int((max(mmd2u_arr) - min(mmd2u_arr))/(max(mmd2u_null_agg) - min(mmd2u_null_agg))*bin1)
+            ax.hist(mmd2u_arr, bins=bin2, alpha=0.5, density=True, color='blue'  , label='$MMD^2_u$ True Distribution', stacked=True)
+            ax.legend()
+            ax.set_xlabel('$MMD^2_u$')
+            ax.set_ylabel('$p(MMD^2_u)$')
+            ax.set_title('$MMD^2_u$: null-distribution and true distribution: with $p$-value=%s'
+          % round(p_value,5))
+            plt.show()
+        return p_value, testOutput
+
+def event2TStest(E_loc, TS, iterations, alphaTest, alphaOmegaset, windowSize, omegaSize, screen=True, returnMinPval=False):
+    """
+      Method designed to 
+
+      Args:
+        omegaSet: np.ndarray or list, population of arrays
+        test_array: np.array, array of values to be tested against omegaSet
+        iterations: int, number of iterartions required to compute MMD^2_u
+        alphaTest: float, acceptance value for hypothesis testing (single array testing)
+        alphaOmegaset: float, acceptance value for hypothesis testing (omegaSet testing)
+        printFlag: bool, flag to plot MMD distribution
+      Return:
+        relation: string, outcome of the event to timeseries temporal analysis
+    """
+
+    omegaSet = omega(windowSize,TS,omegaSize)
+    L_front  = getL(E_loc,windowSize,TS,'front')
+    L_rear   = getL(E_loc,windowSize,TS,'rear')
+    L_front_rear = np.concatenate((L_front, L_rear), axis=None)
+    minPval = 1.0
+
+    print('=============================================')
+    # print('MMD Testing')
+    print("Computing statitics for Lfront vs. omega")
+    p_val_f , test_out_f = MMD_test(L_front, omegaSet, iterations, alphaTest, alphaOmegaset, False)
+    print('Correlated?', test_out_f, 'p value:', p_val_f)
+    if p_val_f < minPval:
+        minPval = p_val_f
+
+    print("\nComputing statistics for Lrear vs. omega")
+    p_val_r , test_out_r = MMD_test(L_rear, omegaSet, iterations, alphaTest, alphaOmegaset, False)
+    print('Correlated?', test_out_r, 'p value:', p_val_r)
+    if p_val_r < minPval:
+        minPval = p_val_r
+
+    print("\nComputing statitics for Lfront vs. Lrear")
+    p_val_3 , test_out_3 = MMD_test(L_rear, L_front, iterations, alphaTest, alphaOmegaset, False)
+    print('Correlated?', test_out_3, 'p value:', p_val_3)
+
+    print("\nComputing statitics for Lfront u Lrear vs. omega")
+    p_val_4, test_out_4 = MMD_test(L_front_rear, omegaSet, iterations, alphaTest, alphaOmegaset, False)
+    print('Correlated?', test_out_4, 'p value:', p_val_4)
+    if p_val_4 < minPval:
+        minPval = p_val_4
+
+    # tscore = t_score(L_front, L_rear)
+
+    relation = None
+
+    ###############################################
+    #   Old detection logic
+    #
+    # if test_out_3==False or test_out_4==False:
+    #     # add more output info here [congjian]
+    #     print('S and E are uncorrelated')
+    #     relation = 'S and E are uncorrelated'
+    # elif test_out_r==True  and test_out_f==False:
+    #     print('E --> S')
+    #     relation = 'E --> S'
+    # elif test_out_r==False and test_out_f==True:
+    #     print('S --> E')
+    #     relation = 'S --> E'
+    # elif test_out_r==True  and test_out_f==True:
+    #     print('S;E')
+    #     relation = 'S;E'
+    #     '''
+    #     if p_val_f>p_val_r:
+    #           print('S -->* E')
+    #     else:
+    #           print('E -->* S')
+    #     '''
+    # else:
+    #     print('S and E are undecided')
+    #     relation = 'S and E are undecided'
+
+    #####################################
+    # New Detection Logic
+    #
+    if test_out_r==True  and test_out_f==False:
+        relation = 'E --> S'
+    elif test_out_r==False and test_out_f==True:
+        relation = 'S --> E'
+    elif test_out_r==True  and test_out_f==True:
+        relation = 'S;E'
+        '''
+        if p_val_f>p_val_r:
+            relation = 'S -->* E'
+        else:
+            relation = 'E -->* S'
+        '''
+    else:
+        if test_out_4:
+            relation = 'S;E'
+        else:
+            if test_out_3:
+                # S and E may be correlated
+                relation = 'S?E'
+            else:
+                # S and E are uncorrelated
+                relation = 'S!E'
+    print('Identified relation: ', relation)
+    if returnMinPval:
+        return relation, minPval
+    else:
+        return relation
+
