ba04acb2b051eda777fbd5c3f2b04c138a6c9a15
Congjian Wang <congjian.wang@inl.gov>
2025-07-31 23:09:12 -0600

    initial setup for dackar main control module

--- src/dackar/main.py
+++ src/dackar/main.py
@@ -1,16 +1,24 @@
 # Copyright 2024, Battelle Energy Alliance, LLC  ALL RIGHTS RESERVED
 
+"""
+Created on July 31, 2025
+@author: wangc, mandd
+"""
+import os
 import argparse
 import logging
 import sys
 
-from .utils import readToml, writeToFile
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from dackar.utils.nlp.CreatePatterns import CreatePatterns
+from dackar.utils.utils import readToml, writeToFile
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 # To enable the logging to both file and console, the logger for the main should be the root,
 # otherwise, a function to add the file handler and stream handler need to be created and called by each module.
 # logger = logging.getLogger(__name__)
-logger = logging.getLogger()
+logger = logging.getLogger('DACKAR')
 # # create file handler which logs debug messages
 fh = logging.FileHandler(filename='dackar.log', mode='w')
 fh.setLevel(logging.DEBUG)
@@ -20,17 +28,19 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 def main():
-
+  logger.info('Welcome!')
   # set up argument parser
   parser = argparse.ArgumentParser(description='DACKAR Input ArgumentParser')
-  parser.add_argument('file_path', type=str, help='The path to the input file.')
+  parser.add_argument('file_path', type=str, default='./system_tests/test_opm.toml' ,help='The path to the input file.')
   parser.add_argument('--output-file', type=str, default='output.txt', help='The file to save the output to.')
   # parse the arguments
   args = parser.parse_args()
+  logger.info('Input file: %s', args.file_path)
   # read the TOML file
   inputDict = readToml(args.file_path)
 
 
+  logger.info(' ... Complete!')
 
 if __name__ == '__main__':
   sys.exit(main())
