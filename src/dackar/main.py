06879dda623adba720860d284e0923efd2a328b0
Congjian Wang <congjian.wang@inl.gov>
2025-08-01 09:39:56 -0600

    initial draft for dackar main

--- src/dackar/main.py
+++ src/dackar/main.py
@@ -8,11 +8,17 @@ import os
 import argparse
 import logging
 import sys
+import spacy
+import pandas as pd
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from dackar.utils.nlp.CreatePatterns import CreatePatterns
 from dackar.utils.utils import readToml, writeToFile
+from dackar.workflows.RuleBasedMatcher import RuleBasedMatcher
+from dackar import config
+from dackar.utils.nlp.nlp_utils import generatePatternList
+# OPL parser to generate object and process lists
+from dackar.utils.opm.OPLparser import OPMobject
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 # To enable the logging to both file and console, the logger for the main should be the root,
@@ -38,6 +44,59 @@ def main():
   logger.info('Input file: %s', args.file_path)
   # read the TOML file
   inputDict = readToml(args.file_path)
+
+  # load nlp model
+  nlp = spacy.load(inputDict['params']['language_model'], exclude=[])
+
+  label = inputDict['params']['ent']['label']
+  entId = inputDict['params']['ent']['id']
+
+  ents = []
+  # Parse OPM model
+  # some modifications, bearings --> pump bearings
+  if 'opm' in inputDict['files']:
+    opmFile = inputDict['files']['opm']
+    opmObj = OPMobject(opmFile)
+    formList = opmObj.returnObjectList()
+    functionList = opmObj.returnProcessList()
+    attributeList = opmObj.returnAttributeList()
+    ents.extend(formList)
+  if 'entity' in inputDict['files']:
+    entityFile = inputDict['files']['entity']
+    entityList = pd.read_csv(entityFile).values.ravel().tolist()
+    ents.extend(entityList)
+
+  ents = set(ents)
+
+  # convert opm formList into matcher patternsOPM
+  patternsOPM = generatePatternList(ents, label=label, id=entId, nlp=nlp, attr="LEMMA")
+  ########################################################################
+  #  Parse causal keywords, and generate patterns for them
+  #  The patterns can be used to identify the causal relationships
+  causalLabel = "causal_keywords"
+  causalID = "causal"
+  patternsCausal = []
+  causalFilename = config.nlpConfig['files']['cause_effect_keywords_file']
+  ds = pd.read_csv(causalFilename, skipinitialspace=True)
+  for col in ds.columns:
+    vars = set(ds[col].dropna())
+    patternsCausal.extend(generatePatternList(vars, label=causalLabel, id=causalID, nlp=nlp, attr="LEMMA"))
+
+  # text that needs to be processed. either load from file or direct assign
+  textFile = inputDict['files']['text']
+  with open(textFile, 'r') as ft:
+    doc = ft.read()
+
+  # doc = "The Pump is not experiencing enough flow for the pumps to keep the check valves open during test."
+
+  name = 'ssc_entity_ruler'
+  matcher = RuleBasedMatcher(nlp, entID=entId, causalKeywordID=causalID)
+  matcher.addEntityPattern(name, patternsOPM)
+
+  causalName = 'causal_keywords_entity_ruler'
+  matcher.addEntityPattern(causalName, patternsCausal)
+
+  matcher(doc.lower())
 
 
   logger.info(' ... Complete!')
