3327d8e2a39f6b4a3a027f1c758f71d1a404937e
Congjian Wang <congjian.wang@inl.gov>
2025-08-01 16:21:55 -0600

    restructure functions

--- src/dackar/workflows/WorkflowManager.py
+++ src/dackar/workflows/WorkflowManager.py
@@ -24,9 +24,39 @@ class WorkflowManager:
     self._nlp = nlp
     self._label = config['params']['ent']['label']
     self._entId = config['params']['ent']['id']
+
+    self._causalLabel = "causal"
+    self._causalID = "causal"
+
     self._patterns = self.processInput(config)
+    self._causalPatterns = self.processCausalEnt()
     self._config = config
 
+    self._workflow = self.setWorflow()
+
+
+  def run(self, doc):
+
+    self._workflow(doc)
+
+  def get(self):
+    pass
+
+  def write(self, fname, style='csv'):
+    pass
+
+  def visualize(self):
+    pass
+
+  def reset(self):
+    pass
+
+
+
+
+############################################
+#      Internal Functions
+############################################
 
   def processInput(self, config):
     ents = []
@@ -50,36 +80,24 @@ class WorkflowManager:
     patterns = generatePatternList(ents, label=self._label, id=self._entId, nlp=self._nlp, attr="LEMMA")
     return patterns
 
-  def run(self, doc):
+  def processCausalEnt(self):
     ########################################################################
     #  Parse causal keywords, and generate patterns for them
     #  The patterns can be used to identify the causal relationships
-    causalLabel = "causal_keywords"
-    causalID = "causal"
+
     patternsCausal = []
     causalFilename = defaultConfig.nlpConfig['files']['cause_effect_keywords_file']
     ds = pd.read_csv(causalFilename, skipinitialspace=True)
     for col in ds.columns:
-      vars = set(ds[col].dropna())
-      patternsCausal.extend(generatePatternList(vars, label=causalLabel, id=causalID, nlp=self._nlp, attr="LEMMA"))
+      cvars = set(ds[col].dropna())
+      patternsCausal.extend(generatePatternList(cvars, label=self._causalLabel, id=self._causalID, nlp=self._nlp, attr="LEMMA"))
+    return patternsCausal
 
+  def setWorflow(self):
     name = 'ssc_entity_ruler'
-    matcher = RuleBasedMatcher(self._nlp, entID=self._entId, causalKeywordID=causalID)
+    matcher = RuleBasedMatcher(self._nlp, entID=self._entId, causalKeywordID=self._causalID)
     matcher.addEntityPattern(name, self._patterns)
 
     causalName = 'causal_keywords_entity_ruler'
-    matcher.addEntityPattern(causalName, patternsCausal)
-
-    matcher(doc)
-
-  def get(self):
-    pass
-
-  def write(self, fname, style='csv'):
-    pass
-
-  def visualize(self):
-    pass
-
-  def reset(self):
-    pass
+    matcher.addEntityPattern(causalName, self._causalPatterns)
+    return matcher
